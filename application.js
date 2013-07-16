e08548014077b55f704734e6eacb91aeeb55553a
Chris Hughes <hughes.c.i@gmail.com>
2013-07-16 19:50:51 +0100

    added draggable pieces

--- application.js
+++ application.js
@@ -3,4 +3,26 @@
 $(document).ready(function() {
 	console.log('js injected');
 
+	$('.piece').draggable({
+		cursor: 'hand',
+		containment: ".board"
+	});
+
+	$('.board .myrow div').droppable({
+		accept: '.piece',
+		hoverClass: 'hover',
+		drop: function(event, ui) {
+
+			console.log(this);
+
+		var old_square = ui.draggable;
+		var new_square = this;
+		// var args = [$(old_square).parent().data('coords').row,$(old_square).parent().data('coords').col,
+		// 	$(new_square).data('coords').row,$(new_square).data('coords').col];
+
+		// console.log(args);
+
+		},
+	});
+
 });
